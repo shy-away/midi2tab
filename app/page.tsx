3e1a46277b98b3384cf6efc44e1733f0661ad927
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-10 17:44:31 -0500

    feat: file upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,80 @@
+"use client";
+
 import styles from "@/app/styles/barberpole-bg.module.scss";
+import { UploadCloudIcon } from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
+  const [fileName, setFileName] = useState<string | null>(null);
+  const [fileArrayBufferCreator, setFileArrayBufferCreator] = useState<
+    (() => Promise<ArrayBuffer>) | null
+  >(null);
+
+  async function handleFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files![0];
+    setFileName(file.name);
+    setFileArrayBufferCreator(file.arrayBuffer);
+  }
+
+  async function handleDrop(event: React.DragEvent<HTMLLabelElement>) {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    setFileName(file.name);
+    setFileArrayBufferCreator(file.arrayBuffer);
+  }
+
   return (
-    <div className={`${styles.barberpoleBg} grow min-h-full`}>
-      Home page
+    <div
+      className={`${styles.barberpoleBg} grow min-h-full flex flex-col items-center`}
+    >
+      {/* Midi Input */}
+      <div className="mt-3 w-11/12 md:w-8/12 lg:max-w-200 rounded-3xl border-2 p-5 backdrop-blur-xs">
+        <form action="">
+          <div className="flex-1 items-center max-w-screen-sm mx-auto mb-3 space-y-4 sm:flex sm:space-y-0">
+            {/*
+              Midi Input Element
+              Modified from: https://tailwindflex.com/@anonymous/file-input
+            */}
+            <div className="relative w-full">
+              <div className="items-center justify-center max-w-xl mx-auto">
+                <label
+                  className="flex justify-center w-full h-32 px-4 transition bg-blend-color border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-white focus:outline-none"
+                  onDragOver={(e) => e.preventDefault()}
+                  onDrop={handleDrop}
+                >
+                  <span className="space-x-2 flex flex-col justify-evenly items-center">
+                    {fileName && <span>Uploaded {fileName}</span>}
+                    <span className="flex items-center space-x-2">
+                      <UploadCloudIcon color="#4a5565" />
+                      {/* matches tailwind's text-gray-600 */}
+                      <span className="font-medium text-gray-600">
+                        Drag and drop, or
+                        <span className="text-blue-600 underline ml-1">
+                          browse
+                        </span>
+                      </span>
+                    </span>
+                  </span>
+                  <input
+                    type="file"
+                    name="file_upload"
+                    className="hidden"
+                    accept="midi"
+                    id="input"
+                    onChange={handleFileUpload}
+                  />
+                </label>
+              </div>
+            </div>
+          </div>
+          <div id="midi-input-divier" className="border-2"></div>
+        </form>
+      </div>
+
+      {/* Presets */}
+      {
+        // TODO: add presets
+      }
     </div>
   );
 }
