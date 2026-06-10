86c167007fc78d13ce6fac8d54b010431f9448f1
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-10 08:44:40 -0500

    feat: background barberpole effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import styles from "@/app/styles/barberpole-bg.module.scss";
+
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className={`${styles.barberpoleBg} grow min-h-full`}>
+      Home page
+    </div>
+  );
 }
