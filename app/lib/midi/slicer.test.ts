fc0899ee637dbaa75fa1e482cc2bce4088ae5bbe
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-23 07:35:37 -0500

    feat: slicer initial return value

--- app/lib/midi/slicer.test.ts
+++ app/lib/midi/slicer.test.ts
@@ -1,15 +1,24 @@
 import slicer from "./slicer";
 
 describe("Slice generator", () => {
-  describe("Basics", () => {
-    const dummyInput = { endTick: 0, notes: [] };
+  const dummyInput = {
+    endTick: 2000,
+    notes: [
+      { pitch: 60, on: 10, off: 480 },
+      { pitch: 60, on: 10, off: 960 },
+      { pitch: 60, on: 0, off: 480 },
+    ],
+  };
 
-    it("should exist", () => {
-      expect(slicer).toBeDefined();
-    });
+  it("should exist", () => {
+    expect(slicer).toBeDefined();
+  });
 
-    it("should return an array", () => {
-      expect(slicer(dummyInput)).toBeInstanceOf(Array);
-    });
+  it("should return an array", () => {
+    expect(slicer(dummyInput)).toBeInstanceOf(Array);
+  });
+
+  it("returns at least one element", () => {
+    expect(slicer(dummyInput).length).toBeGreaterThanOrEqual(1);
   });
 });
--- app/lib/midi/slicer.ts
+++ app/lib/midi/slicer.ts
@@ -10,11 +10,27 @@ export type SlicerInput = {
 };
 
 export type Slice = {
-  notes: { pitch: number; holdover: boolean }[];
+  notes: SliceNote[];
   start: number;
   end: number;
 };
 
-export default function slicer(input: SlicerInput): Slice[] {
-  return [];
+export type SliceNote = {
+  pitch: number;
+  holdover: boolean;
+};
+
+export default function slicer({ endTick, notes }: SlicerInput): Slice[] {
+  // sort by note on asc, breaking ties by note off asc
+  notes.sort((a, b) => (a.on !== b.on ? a.on - b.on : a.off - b.off));
+
+  const active: SliceNote[] = notes
+    .filter((note: SlicerInputNote) => note.on === 0)
+    .map(({ pitch }: SlicerInputNote) => {
+      return { pitch, holdover: false };
+    });
+
+  const result = [{ notes: active, start: 0, end: endTick }];
+
+  return result;
 }
