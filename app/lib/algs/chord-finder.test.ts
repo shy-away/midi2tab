e48a99b7276bf666c60c49da6ddd01994cff2580
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-24 09:09:19 -0500

    feat: init chord-finder with tests

---
+++ app/lib/algs/chord-finder.test.ts
@@ -0,0 +1,9 @@
+import chordFinder from "./chord-finder";
+
+describe("Chord finder", () => {
+  describe("Basics", () => {
+    it("exists", () => {
+      expect(chordFinder).not.toBeUndefined();
+    });
+  });
+});
