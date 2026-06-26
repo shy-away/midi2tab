8a61335848f18c87bd4a14dd2a9984c519eb43b5
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-26 12:30:34 -0500

    feat: define ChordDifficulty

--- app/lib/algs/chord-finder.ts
+++ app/lib/algs/chord-finder.ts
@@ -3,13 +3,14 @@ import { Enumerator, Pitch } from "@/app/lib/types";
 
 export type Chord = {
   fingering: [GuitarString, Fret, Finger][];
-  difficulty: number;
+  difficulty: ChordDifficulty;
 };
 
 export type GuitarString = Enumerator<6, []>;
 export type Finger = Enumerator<4, []> | null;
 export type Fret = Enumerator<25, []>;
 export type HandSpan = Exclude<Enumerator<7, []>, Enumerator<1, []>>;
+export type ChordDifficulty = Enumerator<101, []>;
 
 export type ChordFinderOptions = {
   tuning: Tuning;
