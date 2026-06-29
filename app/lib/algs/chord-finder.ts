87ea81b1577ef4ab005c05654cf8fec56a00c4ac
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-29 05:56:17 -0500

    feat: chord voicing generation

--- app/lib/algs/chord-finder.ts
+++ app/lib/algs/chord-finder.ts
@@ -49,5 +49,68 @@ export default function chordFinder(
 
   // console.log(placements);
 
+  /* Chord voicing generation */
+
+  const voicings: Placement[][] = [];
+
+  (function recursiveBacktrack(
+    currentCandidates: Placement[] = [],
+    placementsIndex: number = 0,
+    currentUsedStrings: GuitarString[] = [],
+    currentMinFret?: Fret,
+    currentMaxFret?: Fret,
+  ) {
+    // base case: placementIndex has reached end of placements, finding a valid path
+    if (placementsIndex === placements.length) {
+      voicings.push(currentCandidates!);
+      return;
+    }
+
+    // first call layer: investigate all possibilities
+    if (currentCandidates.length === 0) {
+      for (const placement of placements[placementsIndex]) {
+        recursiveBacktrack(
+          [placement],
+          placementsIndex + 1,
+          [placement[0]],
+          placement[1],
+          placement[1],
+        );
+      }
+      return;
+    }
+
+    // always false by this point, but silences TS warnings
+    if (!currentMinFret || !currentMaxFret) return;
+
+    // determine min/max available next frets
+    const availableSpan = span - (currentMaxFret - currentMinFret + 1);
+    const minAvailableFret = currentMinFret - availableSpan;
+    const maxAvailableFret = currentMaxFret + availableSpan;
+
+    // search next placements
+    for (const placement of placements[placementsIndex]) {
+      // valid placements satisfy these conditions:
+      // 1. not on used strings
+      // 2. within minAvailableFret and maxAvailableFret OR use fret 0 (open)
+      if (
+        !currentUsedStrings.includes(placement[0]) &&
+        (placement[1] === 0 ||
+          (placement[1] >= minAvailableFret &&
+            placement[1] <= maxAvailableFret))
+      ) {
+        recursiveBacktrack(
+          [...currentCandidates, placement],
+          placementsIndex + 1,
+          [...currentUsedStrings, placement[0]],
+          Math.min(currentMinFret, placement[1]) as Fret,
+          Math.max(currentMaxFret, placement[1]) as Fret,
+        );
+      }
+    }
+  })();
+
+  // console.log(voicings);
+
   return;
 }
