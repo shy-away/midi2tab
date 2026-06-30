025d18f0ca5510c3f7e763ee3f4b620bf9d65569
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-30 12:12:10 -0500

    feat: initial songPathfinder skeleton

---
+++ app/lib/algs/song-pathfinder.ts
@@ -0,0 +1,6 @@
+import { Chord } from "@/app/lib/algs/chord-finder";
+import { Slice } from "@/app/lib/algs/slicer";
+
+export function songPathfinder(slices: Slice[], chords: Chord[][]): Chord[] {
+  return [];
+}
