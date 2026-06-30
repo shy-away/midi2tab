a85c5efccd1336e38cdf2ff78d0aa12e4a48e89b
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-30 14:16:54 -0500

    feat: add baselineTime parameter

--- app/lib/algs/song-pathfinder.ts
+++ app/lib/algs/song-pathfinder.ts
@@ -7,8 +7,13 @@ import { Slice } from "@/app/lib/algs/slicer";
  * **Note:** The indices of `slices` are _assumed_ to correspond with the indices of the first dimension of `chords`. That is, `slice[i]`'s chord options are located in `chords[i]`.
  * @param slices Slices of MIDI notes
  * @param chords Chord options corresponding to each MIDI slice
+ * @param baselineTime Baseline length of time (used when weighting transition costs)
  * @returns `Chord[]` of the optimal path through the chord options
  */
-export function songPathfinder(slices: Slice[], chords: Chord[][]): Chord[] {
+export function songPathfinder(
+  slices: Slice[],
+  chords: Chord[][],
+  baselineTime: number,
+): Chord[] {
   return [];
 }
