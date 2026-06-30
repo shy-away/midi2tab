3a0cccd4de9a747039b57a256a18a84f203fbee7
shy_away <149800599+shy-away@users.noreply.github.com>
2026-06-30 12:22:10 -0500

    chore: document songPathfinder

--- app/lib/algs/song-pathfinder.ts
+++ app/lib/algs/song-pathfinder.ts
@@ -1,6 +1,14 @@
 import { Chord } from "@/app/lib/algs/chord-finder";
 import { Slice } from "@/app/lib/algs/slicer";
 
+/**
+ * Determine the optimal path through the given array of chord options.
+ *
+ * **Note:** The indices of `slices` are _assumed_ to correspond with the indices of the first dimension of `chords`. That is, `slice[i]`'s chord options are located in `chords[i]`.
+ * @param slices Slices of MIDI notes
+ * @param chords Chord options corresponding to each MIDI slice
+ * @returns `Chord[]` of the optimal path through the chord options
+ */
 export function songPathfinder(slices: Slice[], chords: Chord[][]): Chord[] {
   return [];
 }
