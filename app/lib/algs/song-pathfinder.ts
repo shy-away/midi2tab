91fab4b72acd6c855ee31419513ff46e9e986b24
shy_away <149800599+shy-away@users.noreply.github.com>
2026-07-01 11:42:08 -0500

    refactor: define getTransitionCost() in pathfinder

--- app/lib/algs/song-pathfinder.ts
+++ app/lib/algs/song-pathfinder.ts
@@ -15,6 +15,85 @@ export function songPathfinder(
   chords: Chord[][],
   baselineTime: number,
 ): Chord[] {
+  function getTransitionCost(
+    chordA: Chord,
+    chordB: Chord,
+    transitionTime: number,
+  ): number {
+    const { center: chordACenter, fingerStrings: chordAFingerStrings } =
+      fetchChordData(chordA);
+
+    const { center: chordBCenter, fingerStrings: chordBFingerStrings } =
+      fetchChordData(chordB);
+
+    /*
+     * The distance cost is calculated such that its result is in range [0, `maxDistanceCost`], and it increases proportionally with the distance between the chord centers. Distances between 0 and 1 are given a cost of 0; distances over 12 are capped at the maximum cost.
+     */
+
+    const maxDistanceCost = 50;
+    const distanceScalingFactor = maxDistanceCost / 11;
+
+    let distanceCost: number;
+
+    if (!chordACenter || !chordBCenter) {
+      distanceCost = 0;
+    } else {
+      distanceCost =
+        Math.abs(chordACenter - chordBCenter) * distanceScalingFactor -
+        distanceScalingFactor;
+      distanceCost = Math.max(0, Math.min(maxDistanceCost, distanceCost));
+    }
+
+    /*
+     * The fingering cost is calculated based on how the fingerings change between chords. Each finger's usage in the transition from chordA to chordB can be in one of three states: lifts off, gets added, or is used in both. If a finger is used in both chords, it may either stay on the same string or move strings; staying imposes no fingering cost, but moving imposes the maximum per-finger cost.
+     */
+
+    const maxFingeringCost = 50;
+    const maxFingeringCostPerFinger = maxFingeringCost / 4;
+
+    let fingeringCost: number = 0;
+
+    const chordAFingerSet = new Set<Finger>(
+      chordAFingerStrings.map(({ finger }) => finger),
+    );
+
+    const chordBFingerSet = new Set<Finger>(
+      chordBFingerStrings.map(({ finger }) => finger),
+    );
+
+    for (let currentFinger: Finger = 0; currentFinger! < 4; currentFinger!++) {
+      currentFinger = currentFinger as Finger;
+
+      const chordAHasFinger = chordAFingerSet.has(currentFinger);
+      const chordBHasFinger = chordBFingerSet.has(currentFinger);
+
+      // finger lifts off
+      if (chordAHasFinger && !chordBHasFinger) {
+        fingeringCost += maxFingeringCostPerFinger * 0.3;
+      }
+
+      // finger is added
+      else if (!chordAHasFinger && chordBHasFinger) {
+        fingeringCost += maxFingeringCostPerFinger * 0.4;
+      }
+
+      // finger possibly rearticulates
+      else if (chordAHasFinger && chordBHasFinger) {
+        // does the finger move to a new string?
+        if (
+          chordAFingerStrings.find(({ finger }) => finger === currentFinger)!
+            .guitarString !==
+          chordBFingerStrings.find(({ finger }) => finger === currentFinger)!
+            .guitarString
+        ) {
+          fingeringCost += maxFingeringCostPerFinger;
+        }
+      }
+    }
+
+    return 0;
+  }
+
   return [];
 }
 
@@ -37,85 +116,6 @@ function fetchChordData(chord: Chord) {
     };
 
   return cache[cacheKey];
-}
-
-function getTransitionCost(
-  chordA: Chord,
-  chordB: Chord,
-  transitionTime: number,
-): number {
-  const { center: chordACenter, fingerStrings: chordAFingerStrings } =
-    fetchChordData(chordA);
-
-  const { center: chordBCenter, fingerStrings: chordBFingerStrings } =
-    fetchChordData(chordB);
-
-  /*
-   * The distance cost is calculated such that its result is in range [0, `maxDistanceCost`], and it increases proportionally with the distance between the chord centers. Distances between 0 and 1 are given a cost of 0; distances over 12 are capped at the maximum cost.
-   */
-
-  const maxDistanceCost = 50;
-  const distanceScalingFactor = maxDistanceCost / 11;
-
-  let distanceCost: number;
-
-  if (!chordACenter || !chordBCenter) {
-    distanceCost = 0;
-  } else {
-    distanceCost =
-      Math.abs(chordACenter - chordBCenter) * distanceScalingFactor -
-      distanceScalingFactor;
-    distanceCost = Math.max(0, Math.min(maxDistanceCost, distanceCost));
-  }
-
-  /*
-   * The fingering cost is calculated based on how the fingerings change between chords. Each finger's usage in the transition from chordA to chordB can be in one of three states: lifts off, gets added, or is used in both. If a finger is used in both chords, it may either stay on the same string or move strings; staying imposes no fingering cost, but moving imposes the maximum per-finger cost.
-   */
-
-  const maxFingeringCost = 50;
-  const maxFingeringCostPerFinger = maxFingeringCost / 4;
-
-  let fingeringCost: number = 0;
-
-  const chordAFingerSet = new Set<Finger>(
-    chordAFingerStrings.map(({ finger }) => finger),
-  );
-
-  const chordBFingerSet = new Set<Finger>(
-    chordBFingerStrings.map(({ finger }) => finger),
-  );
-
-  for (let currentFinger: Finger = 0; currentFinger! < 4; currentFinger!++) {
-    currentFinger = currentFinger as Finger;
-
-    const chordAHasFinger = chordAFingerSet.has(currentFinger);
-    const chordBHasFinger = chordBFingerSet.has(currentFinger);
-
-    // finger lifts off
-    if (chordAHasFinger && !chordBHasFinger) {
-      fingeringCost += maxFingeringCostPerFinger * 0.3;
-    }
-
-    // finger is added
-    else if (!chordAHasFinger && chordBHasFinger) {
-      fingeringCost += maxFingeringCostPerFinger * 0.4;
-    }
-
-    // finger possibly rearticulates
-    else if (chordAHasFinger && chordBHasFinger) {
-      // does the finger move to a new string?
-      if (
-        chordAFingerStrings.find(({ finger }) => finger === currentFinger)!
-          .guitarString !==
-        chordBFingerStrings.find(({ finger }) => finger === currentFinger)!
-          .guitarString
-      ) {
-        fingeringCost += maxFingeringCostPerFinger;
-      }
-    }
-  }
-
-  return 0;
 }
 
 function getCenter(chord: Chord): number | null {
