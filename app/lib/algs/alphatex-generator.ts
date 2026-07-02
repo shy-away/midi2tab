d9fbc2f4fe38d8756afad5158f85cefb0e52e2eb
shy_away <149800599+shy-away@users.noreply.github.com>
2026-07-02 10:51:36 -0500

    feat: metadata

--- app/lib/algs/alphatex-generator.ts
+++ app/lib/algs/alphatex-generator.ts
@@ -1,11 +1,12 @@
-import { Chord } from "@/app/lib/algs/chord-finder";
+import { Chord, Fret } from "@/app/lib/algs/chord-finder";
 import { Slice } from "@/app/lib/algs/slicer";
-import { Tuning } from "@/app/lib/tunings";
+import { defaultTuning, Tuning } from "@/app/lib/tunings";
+import { midiToNoteName } from "@tonaljs/midi";
 
 export type alphatexOptions = {
   title?: string;
-  tuning?: Tuning;
-  capo?: number;
+  tuning: Tuning;
+  capo?: Fret;
   timeSigTop?: number;
   timeSigBottom?: number;
 };
@@ -13,7 +14,35 @@ export type alphatexOptions = {
 export default function alphatexGenerator(
   slices: Slice[],
   chords: Chord[],
-  { title, tuning, capo, timeSigTop, timeSigBottom }: alphatexOptions,
+  {
+    title = "Untitled",
+    tuning,
+    capo = 0,
+    timeSigTop = 4,
+    timeSigBottom = 4,
+  }: alphatexOptions,
 ): string {
-  return "";
+  const texArr: string[] = [];
+
+  /* Metadata */
+
+  texArr.push(`\\title "${title}"`);
+
+  if (tuning !== defaultTuning) {
+    const pitches = tuning.pitches
+      .reverse()
+      .map((p) => midiToNoteName(p, { sharps: true }));
+
+    texArr.push(`\\tuning (${pitches.join(" ")}) { label "${tuning.name}" }`);
+  }
+
+  if (capo > 0) {
+    texArr.push(`\\capo ${capo}`);
+  }
+
+  texArr.push(`\\ts ${timeSigTop} ${timeSigBottom}`);
+
+  // texArr.push(":4 3.5 5.5 7.5 3.5");
+
+  return texArr.join("\n");
 }
